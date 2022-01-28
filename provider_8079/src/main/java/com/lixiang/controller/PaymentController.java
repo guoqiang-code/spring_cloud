55e8366784d878b53419dbd3c2b7e08eabdc20af
郭强 <guoqiang3@lixiang.com>
2022-01-28 14:09:27 +0800

    'feat-eureka隐藏主机名，显示ip地址，以及DiscoveryClient服务发现'

--- provider_8079/src/main/java/com/lixiang/controller/PaymentController.java
+++ provider_8079/src/main/java/com/lixiang/controller/PaymentController.java
@@ -5,10 +5,17 @@ import com.lixiang.entity.Result;
 import com.lixiang.service.PaymentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import jdk.internal.dynalink.support.AutoDiscovery;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @author GuoQiang
@@ -25,6 +32,10 @@ public class PaymentController {
     @Value("${server.port}")
     private String port;
 
+    //服务检测
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @ApiOperation(value="添加")
     @PostMapping("/provider/insert")
     public Result insert(@RequestBody Payment payment) {
@@ -40,4 +51,16 @@ public class PaymentController {
         Result<Payment> result = new Result<>(200,"成功,serverPort:"+port,payment);
         return result;
     }
+
+    //服务发现
+    @GetMapping("provider/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        services.forEach(log::info);
+        List<ServiceInstance> instances = discoveryClient.getInstances("SPRING-CLOUD-PROVIDER");
+        instances.forEach(item->log.info(item.getServiceId()+"\t"+item.getHost()+"\t"+item.getPort()+"\t"+item.getUri()));
+
+        return this.discoveryClient;
+    }
+
 }
